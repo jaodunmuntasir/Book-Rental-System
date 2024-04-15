20783702694b2167ef736f25dcd4d7891bc1afc8
Muntasir <bvllr5@inf.elte.hu>
2024-04-15 22:36:07 +0200

    New Bootstrap design added. Improved and designed the home page

--- design.md
+++ design.md
@@ -25,3 +25,32 @@ Rentals
         | -------> Completed Rentals
         | -------> Due Rentals
         | -------> View All Renters
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+Book
+    | -------> Add New Book
+    | -------> Book Lists
+Genre
+    | -------> Add New Genre
+    | -------> Genre Lists
+Rental Requests
+    | -------> Pending
+    | -------> Approved 
+    | -------> Rejected
+Rentals List
+    | -------> Ongoing Rentals
+    | -------> Completed Rentals
+    | -------> Due Rentals
+    | -------> View All Rentals
+Renters Requests
+    | -------> Pending
+    | -------> Approved 
+    | -------> Rejected
+Renters List
+    | -------> Ongoing Rentals
+    | -------> Completed Rentals
+    | -------> Due Rentals
+    | -------> View All Renters
+Librarian
+    | -------> Add New Librarian
+    | -------> View All Librarian
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,37 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="p-5 mb-4 bg-body-tertiary rounded-3">
-  <div class="container-fluid py-5">
-    <h1 class="display-4 fw-bold">MyTracks</h1>
-    <p class="col-md-8 fs-4">A place where you can store, edit and view your music projects.</p>
-    <p class="col-md-8 fs-4">We have projects so far.</p>
-    <a class="btn btn-primary btn-lg" type="button" href="#">Learn more</a>
+<!-- Cards for information -->
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card bg-blue text-white mb-4">
+                            <div class="card-header">
+                                Card Title
+                            </div>
+                            <div class="card-body">
+                                This is some text within a card body.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card bg-blue text-white mb-4">
+                            <div class="card-header">
+                                Card Title
+                            </div>
+                            <div class="card-body">
+                                This is some text within a card body.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card bg-blue text-white mb-4">
+                            <div class="card-header">
+                                Card Title
+                            </div>
+       <div the="card-body">
+          This is some text within a card body.
+      </div>
+    </div>
   </div>
 </div>
 @endsection
